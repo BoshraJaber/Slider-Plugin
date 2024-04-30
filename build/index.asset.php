44f70501adbd6c7f7e9aa336ea51d383630d348b
boshrajaber <boshrajaber94@gmail.com>
2024-04-30 18:01:14 +0300

    Slider Plugin

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e1b6c95f96d136cc97e3');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e2eb17c1c853f9e98da7');
